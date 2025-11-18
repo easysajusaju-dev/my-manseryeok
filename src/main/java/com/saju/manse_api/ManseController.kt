b3b8b029410dba19b29bc87c41dc1a9e70b6ba97
easysajusaju-dev <easysaju.saju@gmail.com>
2025-11-18 22:46:25 +0900

    Update ManseController.kt

--- src/main/java/com/saju/manse_api/ManseController.kt
+++ src/main/java/com/saju/manse_api/ManseController.kt
@@ -30,22 +30,22 @@ fun saju(
 // 앱 호환 규칙(정시/동경시-30/절기+10/대운수 floor) 강제
 @GetMapping("/saju/compat")
 fun sajuCompat(
-    @RequestParam("year") year: Int,
-    @RequestParam("month") month: Int,
-    @RequestParam("day") day: Int,
-    @RequestParam("hour") hour: Int,
-    @RequestParam("min") min: Int,
-    @RequestParam("isLunar") isLunar: Boolean,
-    @RequestParam("leap") leap: Boolean,
-    @RequestParam("isMale") isMale: Boolean,
-    @RequestParam("pivotMin", required = false, defaultValue = "30") pivotMin: Int
+    @RequestParam year:Int,
+    @RequestParam month:Int,
+    @RequestParam day:Int,
+    @RequestParam hour:Int,
+    @RequestParam(name="min", defaultValue="0") min:Int,
+    @RequestParam(name="isLunar", defaultValue="false") isLunar:Boolean,
+    @RequestParam(name="leap", defaultValue="false") leap:Boolean,
+    @RequestParam(name="isMale", defaultValue="true") isMale:Boolean
 ): SajuResult {
     return SajuService.getSajuCompat(
         year, month, day, hour, min,
         isLunar, leap, isMale,
-        pivotMin = 30,   // 정답
-        tzAdjust = 0,    // 정답
-        seasonAdjust = 0, // 정답
-        daeRound = "ceil" // 정답
+        pivotMin = 0,      // 정시(0분)
+        tzAdjust = -30,    // 동경시 보정(-30분)
+        seasonAdjust = 0, // 절기 +10분 보정
+        daeRound = "floor" // 대운수 내림
     )
+}
 }
